7aa558b6e043654862dc93a447a3deee4e807beb
chengxiang <chengxiang@phoenixcompany.cn>
2023-02-02 15:13:33 +0800

    DC-60,验证IRQ tree,增加PCIE msi interrupt

--- platform/clounix/sonic-platform-modules-flines/sysfs/bsp_driver/driver/clounix_fpga_clx8000.c
+++ platform/clounix/sonic-platform-modules-flines/sysfs/bsp_driver/driver/clounix_fpga_clx8000.c
@@ -15,7 +15,7 @@
 #include "clounix_fpga_clx8000.h"
 #include "device_driver_common.h"
 
-static int g_fpga_clx8000_loglevel = 0x2;
+static int g_fpga_clx8000_loglevel = 0x3;
 #define FPGA_CLX8000_INFO(fmt, args...) do {                                        \
     if (g_fpga_clx8000_loglevel & INFO) { \
         printk(KERN_INFO "[FPGA_CLX8000][func:%s line:%d]\n"fmt, __func__, __LINE__, ## args); \
@@ -49,6 +49,33 @@ DEFINE_SPINLOCK(fpga_msi_lock);
 static struct notifier_block reboot_nb = {0};
 static struct notifier_block restart_nb = {0};
 
+
+struct int_flag_reg
+{
+   unsigned int int_flag0_addr;
+   unsigned int int_flag1_addr;
+   unsigned int int_flag2_addr;
+};
+
+struct irq_vector
+{
+    int irq_mask_bit;
+    char *irq_name;
+    int mark_type;  /*mark: 1 有细分寄存器要查询；0 无细分寄存器*/
+    struct int_flag_reg flag_regs;
+};
+
+const struct irq_vector fpga_irq_arry[] = {
+    {IRQ_CTRL_RST_PSU_BIT, "psu interrupt",0,{0,0,0}}, /*58*/
+    {IRQ_CTRL_RST_TEMP_BIT, "temperature sensor interruput",0,{0,0,0}},
+    {IRQ_CTRL_RST_PMBUS_BIT, "PMbus alert interruput",0,{0,0,0}},
+    {IRQ_CTRL_RST_USB_BIT, "usb over current  interruput",0,{0,0,0}},
+    {IRQ_CTRL_RST_FAN_BIT, "fan alarm interruput",0,{0,0,0}},
+    {IRQ_CTRL_RST_SPF_PRES_BIT, "sfp present interruput",1,{FPGA_CPLD0_PRS_TRIG_ADDR,FPGA_CPLD1_PRS_TRIG_ADDR,FPGA_QDD_IRQ_TRIG_ADDR}},
+    {IRQ_CTRL_RST_SPF_INT_BIT, "sfp interruput",1,{FPGA_CPLD0_INT_TRIG_ADDR,FPGA_CPLD1_INT_TRIG_ADDR,FPGA_QDD_IRQ_TRIG_ADDR}},
+};
+
+
 #define SYS_LED_BIT (2)
 #define SYS_LED_MASK (0x3)
 #define sys_led_reg_offset (0x200)
@@ -76,25 +103,89 @@ void reset_mux_pca9548(void)
     return;
 }
 EXPORT_SYMBOL_GPL(reset_mux_pca9548);
+/*used for FPGA interrupt register test*/
+int fpga_irq_test(void)
+{
+#if 0
+    unsigned int irq_flag_g,int_flag0,int_flag1,int_flag2;
+    int total_irq,i;
+
+    spin_lock(&fpga_msi_lock);
+    /*pci_read_config_word(pdev, pdev->msi_cap + PCI_MSI_DATA_32, &data);*/
+    /*get interrupt flag*/
+    irq_flag_g = readl(clounix_fpga_base + FPGA_IRQ_TRIG_ADDR);
+    /*clear|disable FPGA interrupt flag*/
+    writel(IRQ_CTRL_DISABLE_INT, clounix_fpga_base + FPGA_IRQ_CTRL_ADDR);
+    /*writel(0x00000000, clounix_fpga_base + FPGA_DEBUG_TRIG_ADDR);*/
+    /*print interrupt information*/
+    total_irq = sizeof(fpga_irq_arry) / sizeof(struct irq_vector);
+    for(i=0; i<total_irq; i++){
+        if( fpga_irq_arry[i].irq_mask_bit & irq_flag_g ){
+            FPGA_CLX8000_ERR("%s: generate %s,irq_flag_g=0x%x.\n", __func__, fpga_irq_arry[i].irq_name,irq_flag_g);
+            if(fpga_irq_arry[i].mark_type==1){
+                int_flag0 = readl(clounix_fpga_base + fpga_irq_arry[i].flag_regs.int_flag0_addr);
+                int_flag1 = readl(clounix_fpga_base + fpga_irq_arry[i].flag_regs.int_flag1_addr);
+                int_flag2 = readl(clounix_fpga_base + fpga_irq_arry[i].flag_regs.int_flag2_addr);
+                FPGA_CLX8000_ERR("cpld0_flag = 0x%x,cpld1_flag = 0x%x,qdd_flag = 0x%x.\n",int_flag0,int_flag1,int_flag2);
+            }
+            
+        }
+    }
+    /*enable FPGA interrupt */
+    writel(IRQ_CTRL_ENABLE_INT, clounix_fpga_base + FPGA_IRQ_CTRL_ADDR);    
+    spin_unlock(&fpga_msi_lock);
+#endif
+    return 0;
+
+}
+EXPORT_SYMBOL_GPL(fpga_irq_test);
+
 
 static irqreturn_t clounix_fpga_irq_hd(int irq, void *dev_id)
 {
     struct pci_dev *pdev = dev_id;
     unsigned short data;
+    unsigned int irq_flag_g,int_flag0,int_flag1,int_flag2;
+    int total_irq,i;
 
     spin_lock(&fpga_msi_lock);
     pci_read_config_word(pdev, pdev->msi_cap + PCI_MSI_DATA_32, &data);
-    FPGA_CLX8000_ERR("%s: %x\n", __func__, data);
+    /*get interrupt flag*/
+    irq_flag_g = readl(clounix_fpga_base + FPGA_IRQ_TRIG_ADDR);
+    FPGA_CLX8000_INFO("%s: generate msi interrupt,irq_flag_g=0x%x.\n", __func__, irq_flag_g);
+    /*clear|disable FPGA interrupt flag*/
+    writel(IRQ_CTRL_DISABLE_INT, clounix_fpga_base + FPGA_IRQ_CTRL_ADDR);
+    /*writel(0x00000000, clounix_fpga_base + FPGA_DEBUG_TRIG_ADDR);*/
+#if 1
+    /*print interrupt information*/
+    total_irq = sizeof(fpga_irq_arry) / sizeof(struct irq_vector);
+    for(i=0; i<total_irq; i++){
+        if( fpga_irq_arry[i].irq_mask_bit & irq_flag_g ){
+            FPGA_CLX8000_INFO("%s: generate %s,irq_flag_g=0x%x.\n", __func__, fpga_irq_arry[i].irq_name,irq_flag_g);
+            if(fpga_irq_arry[i].mark_type==1){
+                int_flag0 = readl(clounix_fpga_base + fpga_irq_arry[i].flag_regs.int_flag0_addr);
+                int_flag1 = readl(clounix_fpga_base + fpga_irq_arry[i].flag_regs.int_flag1_addr);
+                int_flag2 = readl(clounix_fpga_base + fpga_irq_arry[i].flag_regs.int_flag2_addr);
+                FPGA_CLX8000_INFO("cpld0_flag = 0x%x,cpld1_flag = 0x%x,qdd_flag = 0x%x.\n",int_flag0,int_flag1,int_flag2);
+            }
+            
+        }
+    }
+    /*enable FPGA interrupt */
+    writel(IRQ_CTRL_ENABLE_INT, clounix_fpga_base + FPGA_IRQ_CTRL_ADDR);    
+#endif
     spin_unlock(&fpga_msi_lock);
 
     return IRQ_HANDLED;
 }
 
+
+
 static int clounix_fpga_probe(struct pci_dev *pdev, const struct pci_device_id *pci_id)
 {
     int err;
     /*mask interrupt*/
-   #if 0
+   #if 1
     if (pci_find_capability(pdev, PCI_CAP_ID_MSI) == 0) {
         FPGA_CLX8000_ERR("%s[%d] MSI not support.\r\n", __func__, __LINE__);
         return -EPERM;
@@ -121,7 +212,7 @@ static int clounix_fpga_probe(struct pci_dev *pdev, const struct pci_device_id *
         goto err_ioremap;
     } 
     pci_set_drvdata(pdev, clounix_fpga_base);
-#if 0
+#if 1
     FPGA_CLX8000_ERR("support %d msi vector\n", pci_msi_vec_count(pdev));
     err = pci_alloc_irq_vectors(pdev, 1, 1, PCI_IRQ_MSI | PCI_IRQ_AFFINITY);
     if (err < 0) {
@@ -134,6 +225,8 @@ static int clounix_fpga_probe(struct pci_dev *pdev, const struct pci_device_id *
         FPGA_CLX8000_ERR("%s[%d] MSI vector alloc fail.\r\n", __func__, __LINE__);
         goto err_irq;
     }
+    /*reset pcie interrupt core, and enable interrupt*/
+    writel(IRQ_CTRL_CFG_RST_BIT, clounix_fpga_base + FPGA_IRQ_CTRL_ADDR);
 #endif
     reboot_nb.notifier_call = sys_led_reboot_work;
     restart_nb.notifier_call = sys_led_reboot_work;
--- platform/clounix/sonic-platform-modules-flines/sysfs/bsp_driver/driver/clounix_fpga_clx8000.h
+++ platform/clounix/sonic-platform-modules-flines/sysfs/bsp_driver/driver/clounix_fpga_clx8000.h
@@ -28,7 +28,27 @@ struct fpga_device_attribute{
 struct fpga_device_attribute fpga_dev_attr_##_name          \
         = FPGA_ATTR(_name, _mode, _show, _store, _index)
 
+/*interrupt register define*/
+#define FPGA_IRQ_CTRL_ADDR            0x700
+#define FPGA_IRQ_STAT_ADDR            0x704
+#define FPGA_IRQ_TRIG_ADDR            0x708
+#define FPGA_CPLD0_INT_TRIG_ADDR      0x70C
+#define FPGA_CPLD1_INT_TRIG_ADDR      0x710
+#define FPGA_QDD_IRQ_TRIG_ADDR        0x714
+#define FPGA_CPLD0_PRS_TRIG_ADDR      0x718
+#define FPGA_CPLD1_PRS_TRIG_ADDR      0x71C
+#define FPGA_DEBUG_TRIG_ADDR          0x010
 
-
+#define IRQ_CTRL_RST_BIT              31
+#define IRQ_CTRL_CFG_RST_BIT          0x8000007F
+#define IRQ_CTRL_RST_PSU_BIT          0x00000001
+#define IRQ_CTRL_RST_TEMP_BIT         0x00000002
+#define IRQ_CTRL_RST_PMBUS_BIT        0x00000004
+#define IRQ_CTRL_RST_USB_BIT          0x00000008
+#define IRQ_CTRL_RST_FAN_BIT          0x00000010
+#define IRQ_CTRL_RST_SPF_PRES_BIT     0x00000020
+#define IRQ_CTRL_RST_SPF_INT_BIT      0x00000040
+#define IRQ_CTRL_DISABLE_INT          0x0000007F
+#define IRQ_CTRL_ENABLE_INT           0x00000000
 
 #endif
